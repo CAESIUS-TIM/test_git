c5279bc538a6f9d48c8a78a92f166373f5a072cf
CAESIUS_TIM <60285058+CAESIUS-TIM@users.noreply.github.com>
2021-05-13 10:40:11 +0800

    format: detele import never used

--- java_test/thisAndSuper/Student.java
+++ java_test/thisAndSuper/Student.java
@@ -2,9 +2,6 @@ package java_test.thisAndSuper;
 
 import java.util.function.*;
 
-import java_test.thisAndSuper.impl.StudentImpl;
-import java_test.thisAndSuper.impl.Undergraduate;
-
 public interface Student{
     String getInfo();
     void info(Consumer<String> consumer);
